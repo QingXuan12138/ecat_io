12256beefebeea192f3cc365edf1c359e4942af6
QingXuan12138 <908597992@qq.com>
2026-03-03 08:24:14 +0800

    1

--- hpm5e00evk_flash_xip_debug/segger_embedded_studio/Output/Debug/Obj/ecat_io - hpm5e00evk/soc/HPM5E00/HPM5E31/toolchains/segger/startup.s.d
+++
@@ -1,4 +0,0 @@
-Output/Debug/Obj/ecat_io\ -\ hpm5e00evk/soc/HPM5E00/HPM5E31/toolchains/segger/startup.s.o: \
-  C:\sdk_env_v1.11.0\hpm_sdk\soc\HPM5E00\HPM5E31\toolchains\segger\startup.s \
-  ..\..\..\..\..\soc\HPM5E00\HPM5E31\hpm_csr_regs.h \
-  C:\sdk_env_v1.11.0\hpm_sdk\soc\HPM5E00\HPM5E31\toolchains\segger\..\vectors.h
--- hpm5e00evk_flash_xip_debug/segger_embedded_studio/Output/Debug/Obj/ecat_io - hpm5e00evk/utils/hpm_crc32.c.d
+++
@@ -1,8 +0,0 @@
-Output/Debug/Obj/ecat_io\ -\ hpm5e00evk/utils/hpm_crc32.c.o: \
- C:\sdk_env_v1.11.0\hpm_sdk\utils\hpm_crc32.c \
- C:\sdk_env_v1.11.0\hpm_sdk\utils\hpm_crc32.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.26b/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.26b/include/__SEGGER_RTL.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.26b/include/__SEGGER_RTL_ConfDefaults.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.26b/include/__SEGGER_RTL_Conf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ 8.26b/include/__SEGGER_RTL_RISCV_Conf.h
